723f7be4bb0821e15c8f1b4e88d88eb94783f8af
alaposhin <alaposhin@gmail.com>
2024-04-08 17:18:15 +0300

    fix: project layout

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,20 @@ import VTab from './components/tabs/VTab.vue'
 </script>
 
 <template>
-  <h1 class="text-2xl font-bold my-8">Examples of Vue 3 components for copy-pasting into your project 😉</h1>
-  <h2 class="text-xl font-bold my-8">Tabs</h2>
-  <VTabs>
-    <VTab title="Tab 1">Content 1</VTab>
-    <VTab title="Tab 2">Content 2</VTab>
-    <VTab title="Tab 3">Content 3</VTab>
-  </VTabs>
+  <div class="flex flex-col justify-between max-w-screen-sm mx-auto min-h-screen py-10">
+    <div>
+      <h1 class="text-2xl font-bold text-center mb-10">
+        Examples of Vue 3 components for copy-pasting into your project 😉
+      </h1>
+      <h2 class="text-xl font-bold mb-8">Tabs</h2>
+      <VTabs>
+        <VTab title="Tab 1">Content 1</VTab>
+        <VTab title="Tab 2">Content 2</VTab>
+        <VTab title="Tab 3">Content 3</VTab>
+      </VTabs>
+    </div>
+    <div class="text-center">
+      View code on my <a href="https://github.com/leshkin/vue-3-examples" class="underline">GitHub</a>
+    </div>
+  </div>
 </template>
